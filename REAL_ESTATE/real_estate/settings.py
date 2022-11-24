58f438693b8cc651bea2f914e6a8030d95312600
Gayatri Kolekar <gayatri.kolekar@gmail.com>
2022-11-23 16:27:36 -0800

    Removed email from settings

--- REAL_ESTATE/real_estate/settings.py
+++ REAL_ESTATE/real_estate/settings.py
@@ -154,12 +154,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='gayatri.kolekar@gmail.com'
-EMAIL_HOST_PASSWORD='uwcgbzseqksuinwk'
-EMAIL_USE_TLS=True
+
 
 
 try:
